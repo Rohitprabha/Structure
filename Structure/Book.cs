3278f4e3b9652c679539facdf9faac7c800bfcf8
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-18 22:27:08 +0530

    Add files via upload

---
+++ Structure/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structure
+{
+    public struct Book
+    {
+        private string title;
+        private string author;
+        private string subject;
+        private int book_id;
+
+        public void getValues(string t, string a, string s, int id)
+        {
+            title = t;
+            author = a;
+            subject = s;
+            book_id = id;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Title : {0}", title);
+            Console.WriteLine("Author : {0}", author);
+            Console.WriteLine("Subject : {0}", subject);
+            Console.WriteLine("Book_id :{0}", book_id);            
+        }
+    };
+}
---
+++ Structure/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book Book1 = new Book();
+            Book Book2 = new Book();
+            Book1.getValues("C Programming", "abc", "C Programming", 6495407);
+            Book2.getValues("C# Programming", "abc", "C#", 6495700);
+            Book1.display();
+            Book2.display();
+
+            Console.ReadKey();
+        }
+    }
+}
